e2abc135216757f1f1021b5c7256c92e63dcbd0b
Spandan Kottakota <74162445+TrueDefault@users.noreply.github.com>
2026-01-30 22:52:37 -0800

    Completed Exercise 2

--- week1_exercise2.cpp
+++ week1_exercise2.cpp
@@ -4,11 +4,24 @@
 //IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
+AnalogIn pot(p15); 
+DigitalOut led(LED1);
+
 int main() {
+    float total_period_ms = 2000.0f; 
+
     while (1) {
         printf("Week 1 Exercise 2");
+        
+        float percentage = pot.read();
 
-        // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
-        wait_ms(500); 
+        int on_time = percentage * total_period_ms;
+        int off_time = (1.0f - percentage) * total_period_ms;
+
+        led = 1;
+        wait_ms(on_time);
+
+        led = 0;
+        wait_ms(off_time);
     }
 }
